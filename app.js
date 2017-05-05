dae4112a91a2c8905a6844c4634c1c893d592c7a
Luke Guild <luke.guild@gmail.com>
2017-05-05 17:06:48 -0400

    update dep to reflect node 6.10

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ gw.getAuthSessionTicket(function(ticket) {
 
   http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('ticket: ' + ticketStr + '\n\nsteam id: ' + steamId);
+    response.end('ticket: ' + ticketStr + '\n\nsteam id: ' + steamId.getRawSteamID() + ', ' + steamId.getAccountID());
   }).listen(8081);
   console.log('Server running at http://127.0.0.1:8081/');
 }, function(err) {
