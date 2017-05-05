fd49142213cfe5f4d91735471ae211451e91c5b5
Luke Guild <luke.guild@gmail.com>
2017-05-05 16:28:43 -0400

    get token and steamid info

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var gw = require("./greenworks");
 
 gw.init();
 
-greenworks.getAuthSessionTicket(function(ticket) {
+gw.getAuthSessionTicket(function(ticket) {
+  const ticketStr = ticket.ticket.toString('hex');
+  const steamId = gw.getSteamId();
+
   http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(ticket);
+    response.end('ticket: ' + ticketStr + '\n\nsteam id: ' + steamId);
   }).listen(8081);
   console.log('Server running at http://127.0.0.1:8081/');
-}, function(err) { throw err; });
-
-// Console will print the message
+}, function(err) {
+  throw err;
+});
